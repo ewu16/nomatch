a8453535767ca3f2100cf287f45955739f0376a1
Emily Wu <emily.wu2@emory.edu>
2026-02-19 16:25:29 -0500

    allow formulas to adjust for only a subset of specified covariates but prevent formulas from adjusting for variables not specified in covariates

--- R/validate_formulas.R
+++ R/validate_formulas.R
@@ -22,16 +22,32 @@ resolve_hazard_formulas <- function(formula_unexposed, formula_exposed, covariat
 
 
 validate_formulas <- function(formula_unexposed, formula_exposed, covariates, exposure_time){
-    if(any(!covariates %in% get_rhs_vars(formula_unexposed))){
-        warning("Formula for unexposed does not include all variables in `covariates`")
+    unexposed_vars <- get_rhs_vars(formula_unexposed)
+    exposed_vars <- get_rhs_vars(formula_exposed)
+    
+    extra_unexposed <- setdiff(unexposed_vars, covariates)
+    extra_exposed <- setdiff(exposed_vars, c(covariates, exposure_time))
+    
+    if(length(extra_unexposed) != 0){
+        stop("`formula_unexposed` contains terms that are not included in `covariates`: ", paste(extra_unexposed, collapse = ", "), call. = FALSE)
+    }
+    if(length(extra_exposed) != 0){
+        stop("`formula_exposed` contains terms that are not included in `covariates`: ", paste(extra_exposed, collapse = ", "), call. = FALSE)
     }
     
-    if(any(!covariates %in% get_rhs_vars(formula_exposed))){
-        warning("Formula for exposed does not include all variables in `covariates`")
+    missing_unexposed <-  setdiff(covariates, unexposed_vars)
+    missing_exposed   <-  setdiff(covariates, exposed_vars)
+
+    if(length(missing_unexposed) != 0){
+        warning("`formula_unexposed` does not include a term for the following `covariates`: ", paste(missing_unexposed, collapse = ","), call.= FALSE)
+    }
+    
+    if(length(missing_exposed) != 0){
+        warning("`formula_exposed` does not include a term for the following `covariates`: ", paste(missing_exposed, collapse = ", "), call.= FALSE)
     }
     
     if(!exposure_time %in% get_rhs_vars(formula_exposed)){
-        warning("Formula for exposed does not include a term for `exposure_time`")
+        warning("`formula_exposed` does not include a term for `exposure_time`",  call.= FALSE)
     }
     
     invisible(NULL)
